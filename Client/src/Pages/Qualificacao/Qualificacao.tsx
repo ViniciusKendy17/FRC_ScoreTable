5657c7770e423a5ee4d222e57d088924ebc568be
LaPachec <lucasaraujox@hotmail.com>
2025-10-17 16:11:49 -0300

    Update Qualificacao.tsx

--- Client/src/Pages/Qualificacao/Qualificacao.tsx
+++ Client/src/Pages/Qualificacao/Qualificacao.tsx
@@ -3,8 +3,36 @@ import Header from "../../Components/Header";
 import TeamBox from "../../Components/TeamBox";
 import styles from "../../Styles/Qualificacao.module.css";
 import Placar from '../../Components/Placar';
+import { useEffect, useState } from 'react';
 
 export default function Qualificacao() {
+
+  const endpoint = 'http://172.25.10.14:3000/frc/';
+  const [data, setData] = useState<any[]>([]);
+
+  const getMatches = async () => {
+    try {
+      const response = await fetch(endpoint + "matches");
+      if (!response.ok)
+        throw new Error(`Erro na requisição: ${response.status}`);
+
+      const json = await response.json();
+      setData(json); // <-- atualiza o estado
+      console.log(json);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("Falha ao buscar matches:", error.message);
+      } else {
+        console.error("Falha ao buscar matches:", String(error));
+      }
+    }
+  };
+
+  useEffect(() => {
+    getMatches();
+  }, []);
+
+
   return (
     <div className={styles.container}>
       <Header title="Qualificatoria" />
